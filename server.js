194425666f5599dddc9aa1c2db714db942e4af49
zeethonSE <zeethon0@gmail.com>
2025-04-09 16:51:21 +0700

    deploy on Render

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // ✅ Use the imported `postRoutes`
 app.use("/posts", postRoutes);
