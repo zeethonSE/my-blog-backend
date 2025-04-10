70e7a612ea6cb522b62db3a9e901545fb3c28b5b
zeethonSE <zeethon0@gmail.com>
2025-04-10 14:48:29 +0700

    update deploy

--- server.js
+++ server.js
@@ -3,10 +3,30 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-app.use(cors({ origin: "https://frontend-anksl0zu4-zeethons-projects.vercel.app" }));
 app.use(express.json());
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || origin.includes("vercel.app")) {
+      // Allow any Vercel-originated frontend
+      callback(null, true);
+    } else {
+      // Deny requests from non-Vercel origins (for security)
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+
 const PORT = process.env.PORT || 10000;
+
+//frontend hitting the server
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // ✅ Use the imported `postRoutes`
 app.use("/posts", postRoutes);
